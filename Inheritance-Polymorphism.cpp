3c08ee75538c6dfadb8dc839ff80e7eea2e0039e
Amirhossein <amirabdolii@gmail.com>
2022-09-15 12:13:08 +0430

    operator overloading added

--- Inheritance-Polymorphism.cpp
+++ Inheritance-Polymorphism.cpp
@@ -4,6 +4,13 @@
 
 using namespace std;
 
+string operator*(int count, string str){
+    string ans = "";
+    while(count--)
+        ans += str;
+    return ans;
+}
+
 class Person {
     private:
         string firstName;
@@ -36,12 +43,63 @@ class Student: public Person {
         }
 };
 
+class Teacher: public Person {
+    private:
+        int workExprince;
+        vector<string> courses;
 
+    public:
+        Teacher(string _firstName, string _lastName, int _age, int _id, int _workExprience):
+        Person(_firstName, _lastName, _age, _id){
+            workExprince = _workExprience;
+        }
+};
 
+class Course {
+    private:
+        string name;
+        string grade;
+        vector< Teacher* > teachers;
+        string description;
+
+    public:
+        Course(string _name, string _grade, string _description = "Not Defined!"){
+            name = _name;
+            grade = _grade;
+            description = _description;
+        }
+        void add_description(string _description){description = _description;}
+
+        void describe(){cout << "*** " << name << " ***" << endl <<
+         description << endl << "*** " + operator*(name.size(), "*") + " ***" << endl;}
+
+};
+
+class MyInt {
+    private:
+        int value;
+    
+    public:
+        string operator*(string str){
+            string ans = "";
+            int val = value;
+            while(val--)
+                ans += str;
+            return ans;
+        }
+        MyInt(int _value){value = _value;}
+};
 
 int main(){
 
     Student* student = new Student("Hasan", "KKNJ", 17, 0023, "11th");
-    student->intro();
+    Course* course = new Course("math", "11th");
+    course->describe();
+    course->add_description("this is the most valuable course in school!");
+    course->describe();
+    int n;
+    cin >> n;
+    MyInt a(n);
+    cout << a * "hasan" << endl;
     return 0;
 }
